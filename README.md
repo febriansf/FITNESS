3a2c4a116d4221da78da9cc7015281812135ec25
Febrian SF <febriansf@student.uns.ac.id>
2022-06-29 00:59:38 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# UAS-DesainWeb
+
+## Status = *On Going*
+
+## State 
+           - Kerangka
+           - .....
+           - Finish - Maybe
+           
+*Note : Tinggalkan pesan jika melakukan perubahan pada repository ini*
---
+++ js/custom.js
@@ -0,0 +1,49 @@
+// Function for hide Navbar while scrolling
+$(function() {
+    let prevScrollPos = window.pageYOffset;
+    $(window).scroll(function() {
+        $('.navbar-collapse').collapse('hide');
+        let currentScrollPos = window.pageYOffset;
+        if (prevScrollPos > currentScrollPos) {
+            $('#navbar').css({'top':'0'});
+        } else {
+            $('#navbar').css({'top':'-90px'});
+        }
+        prevScrollPos = currentScrollPos;
+    });
+});
+
+//Function for Animate Navbar Toggler button
+$(function() {
+    let p = 0
+    $('.navbar-toggler').click(function() {
+        p += 90
+        p = (p > 270) ? p = 0 : p
+        $('.navbar-toggler-icon').css({
+            'transform' : `rotate(${p}deg)`,
+            'transition' : 'transform 500ms'
+        });
+    });
+});
+
+//Function for Show Modal when submit Join registration
+$('#registration').on('submit', function(e) {
+    e.preventDefault(); //stop submit
+    
+    if ($('#checkbox').is(':checked')) {
+    $('#modelId').modal('show');
+    }
+
+    $(this)[0].reset();
+});
+
+//Same as above but this is for Message Form cuz idk how to write a code :v
+$('#message').on('submit', function(e) {
+    e.preventDefault(); //stop submit
+    
+    if ($('#checkbox').is(':checked')) {
+    $('#modelId').modal('show');
+    }
+
+    $(this)[0].reset();
+});
